b6f2d86012dff69bfabde4eaec1ad44466698333
lilrooness <jfrangoudes@gmail.com>
2013-12-03 12:25:22 +0000

    Added all 8 bit adds and subs

--- src/z80/instructions/set/Arith8Bit.java
+++ src/z80/instructions/set/Arith8Bit.java
@@ -2,6 +2,7 @@ package z80.instructions.set;
 
 import z80.core.RegisterState;
 import z80.core.StatusFlags;
+import z80.instructions.AbstractRegisterInstruction;
 import z80.memory.Memory;
 import z80.util.RadixOperations;
 
@@ -18,6 +19,7 @@ public class Arith8Bit {
         RegisterState registerState = RegisterState.getInstance();
         byte n = registerState.fetchWord8();
         int result = registerState.getA()[0] + n;
+        registerState.setA(new byte[] {(byte)result, registerState.getA()[1]});
         RegisterState.psr.set(StatusFlags.S.getPosition(), (result < 0));
         RegisterState.psr.set(StatusFlags.Z.getPosition(), (result == 0));
         RegisterState.psr.set(StatusFlags.UN.getPosition(), false);
@@ -25,7 +27,10 @@ public class Arith8Bit {
     }
 
     public static void AddAr() {
-
+        RegisterState registerState = RegisterState.getInstance();
+        String opcode = RadixOperations.prependZeros(Integer.toBinaryString(registerState.getCurrentWord8()));
+        byte r = AbstractRegisterInstruction.getRegisterValueByCode(opcode.substring(5), registerState)[0];
+        registerState.setA(new byte[]{(byte) (registerState.getA()[0]+r), registerState.getA()[1]});
     }
 
     public static void addAHL() {
@@ -35,5 +40,63 @@ public class Arith8Bit {
         String index = nHigh + nLow;
         byte n = Memory.memory[RadixOperations.toShort(index)];
         registerState.setA(new byte[] {(byte) (registerState.getA()[0] +n),registerState.getA()[1]});
+    }
+
+    public static int addIXd() {
+        RegisterState registerState = RegisterState.getInstance();
+        byte d = registerState.fetchWord8();
+        short a = (short) (registerState.getIX() + d);
+        registerState.setA(new byte[]{(byte) (registerState.getA()[0] + Memory.memory[a]), registerState.getA()[1]});
+        return 5;
+    }
+
+    public static int addIYd() {
+        RegisterState registerState = RegisterState.getInstance();
+        byte d = registerState.fetchWord8();
+        short a = (short) (registerState.getIY() + d);
+        registerState.setA(new byte[]{(byte) (registerState.getA()[0] + Memory.memory[a]), registerState.getA()[1]});
+        return 5;
+    }
+
+    public static int subr() {
+        RegisterState registerState = RegisterState.getInstance();
+        String opcode = RadixOperations.prependZeros(Integer.toBinaryString(registerState.getCurrentWord8()));
+        byte r = AbstractRegisterInstruction.getRegisterValueByCode(opcode.substring(5), registerState)[0];
+        registerState.setA(new byte[]{(byte) (registerState.getA()[0]-r), registerState.getA()[1]});
+        return 1;
+    }
+
+    public static int subn() {
+        RegisterState registerState = RegisterState.getInstance();
+        byte n = registerState.fetchWord8();
+        int result = registerState.getA()[0] - n;
+        registerState.setA(new byte[] {(byte)result, registerState.getA()[1]});
+        return 2;
+    }
+
+    public static int subHL() {
+        RegisterState registerState = RegisterState.getInstance();
+        String nHigh = RadixOperations.prependZeros(Integer.toBinaryString(registerState.getHl()[0] & 0xff));
+        String nLow = RadixOperations.prependZeros(Integer.toBinaryString(registerState.getHl()[1] & 0xff));
+        String index = nHigh + nLow;
+        byte n = Memory.memory[RadixOperations.toShort(index)];
+        registerState.setA(new byte[] {(byte) (registerState.getA()[0] - n),registerState.getA()[1]});
+        return 2;
+    }
+
+    public static int subIXd() {
+        RegisterState registerState = RegisterState.getInstance();
+        byte d = registerState.fetchWord8();
+        short a = (short) (registerState.getIX() + d);
+        registerState.setA(new byte[]{(byte) (registerState.getA()[0] - Memory.memory[a]), registerState.getA()[1]});
+        return 5;
+    }
+
+    public static int subIYd() {
+        RegisterState registerState = RegisterState.getInstance();
+        byte d = registerState.fetchWord8();
+        short a = (short) (registerState.getIY() + d);
+        registerState.setA(new byte[]{(byte) (registerState.getA()[0] - Memory.memory[a]), registerState.getA()[1]});
+        return 5;
     }
 }

051be81268bde2bc47ec51dfa307de99ee994c98
lilrooness <jfrangoudes@gmail.com>
2014-02-16 17:34:42 +0000

    Added file loading functionality to assembler module

--- src/z80/modules/impls/assembler/AssemblerGUI.java
+++ src/z80/modules/impls/assembler/AssemblerGUI.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 /**
  * Created by Joe on 08/02/2014.
@@ -15,6 +16,9 @@ public class AssemblerGUI extends JFrame {
     private JTextArea jTextArea;
     private JButton compile;
     private Assembler assembler;
+    private JMenuBar jMenuBar;
+    private JMenu file;
+    private JMenuItem load;
 
     public AssemblerGUI() {
         setTitle("Z80 Assembler Module");
@@ -24,8 +28,40 @@ public class AssemblerGUI extends JFrame {
         jTextArea = new JTextArea();
         compile = new JButton("Compile and Load");
 
+        jMenuBar = new JMenuBar();
+        file = new JMenu("File");
+        jMenuBar.add(file);
+        load = new JMenuItem("Load");
+        file.add(load);
+
+        load.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JFileChooser fileChooser = new JFileChooser();
+                int status = fileChooser.showOpenDialog(null);
+                if(status == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        BufferedReader in = new BufferedReader(new FileReader(new File(fileChooser.getSelectedFile().getAbsolutePath())));
+                        String line;
+
+                        while((line = in.readLine()) != null) {
+                            jTextArea.append(line+"\n");
+                        }
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
         add(jTextArea, BorderLayout.CENTER);
         add(compile, BorderLayout.SOUTH);
+        add(jMenuBar, BorderLayout.NORTH);
+
+
 
         assembler = new Assembler();
 
